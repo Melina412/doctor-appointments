6cac5f6634708dffb5ac5618695839591944124a
Melina412 <melina.schild@icloud.com>
2024-06-24 15:04:30 +0200

    get route update

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,14 @@ import { app } from './src/app.js';
 import { connect } from './src/config/storage.config.js';
 
 // test um im browser zu checken ob der server erreichbar ist
-app.get('/', (_, res) => {
-  res.json({ message: `express server auf port ${process.env.PORT} ✅` });
-});
+// app.get('/', (_, res) => {
+//   res.json({ message: `express server auf port ${process.env.PORT} ✅` });
+// });
 
 const FRONTEND_INDEX = new URL('../frontend/dist/index.html', import.meta.url)
   .pathname;
 
-app.get('*', (req, res) => {
+app.get('*', (_, res) => {
   res.sendFile(FRONTEND_INDEX);
 });
 
